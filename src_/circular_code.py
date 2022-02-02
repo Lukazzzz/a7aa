517a4b18aa2cdad34dbd5a972c9ec53792ff8fe2
Luka <alaa.tabei94@gmail.com>
2022-02-02 17:13:12 +0100

    angle figured out

--- src_/circular_code.py
+++ src_/circular_code.py
@@ -3,20 +3,35 @@ import matplotlib.pyplot as plt
 import math
 import numpy as np
 
-def calc_radius(base, rad):
-    return 25*math.sin(5*rad) + base
+def calc_radius(base, rad, no_periods, open_angle, amp):
+    waves_per_complete_circle = (360/open_angle) * no_periods
+    curve_angle = waves_per_complete_circle*rad
+    while curve_angle < 0:
+        curve_angle = curve_angle + 2*math.pi
+    while curve_angle > 2*math.pi:
+        curve_angle = curve_angle - 2*math.pi
+    print(math.degrees(curve_angle))
+    return amp*math.sin(waves_per_complete_circle*rad) + base
 
-radius = 100.0
+max_radius = 23.6
+min_radius = 17.6
+# This is the radius which the sine waves go around
+radius = (max_radius+min_radius)/2
+
+open_angle = 72
+open_angle_rad = math.radians(open_angle)
+phase_shift = 120
+
+amplitude = (max_radius-min_radius)/2
+
+# The coordinates of the center point
 center_x = 0
 center_y = 0
+# The step the change in the angle
 step = (2*math.pi)/360
-x_points = []
-y_points = []
-i = -math.pi/5
-x0 = calc_radius(radius, i) * math.sin(i)
-y0 = calc_radius(radius, i) * math.cos(i)
-x_points.append(x0)
-y_points.append(y0)
+
+start_angle = -open_angle_rad/2
+end_angle = open_angle_rad/2
 
 
 fig = plt.figure()
@@ -33,68 +48,46 @@ ax.spines['top'].set_color('none')
 # Show ticks in the left and lower axes only
 ax.xaxis.set_ticks_position('bottom')
 ax.yaxis.set_ticks_position('left')
-
-
-
-
-
-while i < math.pi/5:
-    i = i + step
-    x1 = calc_radius(radius, i) * math.sin(i)
-    y1 = calc_radius(radius, i) * math.cos(i)
-    x_points.append(x1)
-    y_points.append(y1)
-plt.plot(x_points, y_points)
-
-x_points = []
-y_points = []
-i = -math.pi/5
-x0 = calc_radius(radius, i-math.pi/6) * math.sin(i)
-y0 = calc_radius(radius, i-math.pi/6) * math.cos(i)
-x_points.append(x0)
-y_points.append(y0)
-while i < math.pi/5:
-    i = i + step
-    x1 = calc_radius(radius, i-math.pi/6) * math.sin(i)
-    y1 = calc_radius(radius, i-math.pi/6) * math.cos(i)
-    x_points.append(x1)
-    y_points.append(y1)
-plt.plot(x_points, y_points)
-
-x_points = []
-y_points = []
-i = -math.pi/5
-x0 = calc_radius(radius, i-math.pi/6-math.pi/6) * math.sin(i)
-y0 = calc_radius(radius, i-math.pi/6-math.pi/6) * math.cos(i)
-x_points.append(x0)
-y_points.append(y0)
-while i < math.pi/5:
-    i = i + step
-    x1 = calc_radius(radius, i-math.pi/6-math.pi/6) * math.sin(i)
-    y1 = calc_radius(radius, i-math.pi/6-math.pi/6) * math.cos(i)
-    x_points.append(x1)
-    y_points.append(y1)
-plt.plot(x_points, y_points)
-
-angle = np.linspace( 0 , 2 * np.pi , 150 )
-
-radius = 100
-x_circle = radius * np.cos(angle)
-y_circle = radius * np.sin(angle)
-plt.plot(x_circle, y_circle)
-
-radius = 125
-x_circle = radius * np.cos(angle)
-y_circle = radius * np.sin(angle)
-plt.plot(x_circle, y_circle)
-
-radius = 75
-x_circle = radius * np.cos(angle)
-y_circle = radius * np.sin(angle)
-plt.plot(x_circle, y_circle)
-
-
-
-
 ax.set_aspect('equal')
+
+for i in range(0, 360, phase_shift):
+    x_points = []
+    y_points = []
+    angle = start_angle
+    x_start = calc_radius(radius, start_angle-math.radians(i), 1, open_angle, amplitude) * math.sin(start_angle)
+    y_start = calc_radius(radius, start_angle-math.radians(i), 1, open_angle, amplitude) * math.cos(start_angle)
+    x_points.append(x_start)
+    y_points.append(y_start)
+
+    while angle < end_angle:
+        angle = angle + step
+        x_new = calc_radius(radius, angle-math.radians(i), 1, open_angle, amplitude) * math.sin(angle)
+        y_new = calc_radius(radius, angle-math.radians(i), 1, open_angle, amplitude) * math.cos(angle)
+        x_points.append(x_new)
+        y_points.append(y_new)
+        #print(math.degrees(angle-math.radians(i)))
+        #print(x_new, y_new)
+    print('\n\n')
+    plt.plot(x_points, y_points)
+    #plt.show()
+
+
+
+angle = np.linspace(0, 2 * np.pi , 150)
+
+circle_radius = radius
+x_circle = circle_radius * np.cos(angle)
+y_circle = circle_radius * np.sin(angle)
+plt.plot(x_circle, y_circle)
+
+# circle_radius = max_radius
+# x_circle = circle_radius * np.cos(angle)
+# y_circle = circle_radius * np.sin(angle)
+# plt.plot(x_circle, y_circle)
+#
+# circle_radius = min_radius
+# x_circle = circle_radius * np.cos(angle)
+# y_circle = circle_radius * np.sin(angle)
+# plt.plot(x_circle, y_circle)
+
 plt.show()
